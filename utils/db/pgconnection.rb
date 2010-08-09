76f0b44ba128d1bd9934a8888c36680396ae9469
vit <none@none>
2010-08-09 15:51:12 +0000

    added methods query_map & query_inject

--- utils/db/pgconnection.rb
+++ utils/db/pgconnection.rb
@@ -28,19 +28,43 @@ module Raser
 			def query_one q, *args
 				res = query q, *args
 				#p res
-				r = (res && res.first) ? correct_row(res.first) : nil
+				row = (res && res.first) ? correct_row(res.first) : nil
 				res.clear if res
-				r
+				#r
+				(block_given? ? (yield row) : row)
 			end
 			def query_each q, *args
 				res = query q, *args
 				if res
 					res.each do |row|
 						correct_row row
-						yield row
+						yield row if block_given?
 					end
 					res.clear
 				end
+			end
+			def query_map q, *args
+				arr = []
+				res = query q, *args
+				if res
+					res.each do |row|
+						correct_row row
+						arr << (block_given? ? (yield row) : row)
+					end
+					res.clear
+				end
+				arr
+			end
+			def query_inject acc, q, *args
+				res = query q, *args
+				if res
+					res.each do |row|
+						correct_row row
+						acc = (yield acc, row) if block_given?
+					end
+					res.clear
+				end
+				acc
 			end
 			def escape_string s
 				s.to_s.gsub!( /[\']/ ) { |c| "''" }
